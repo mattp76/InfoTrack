d4a4512f6e1faff9005578f683b08e4d024b5442
Matthew Parker <mattp_76@hotmail.com>
2016-07-07 21:43:13 +1000

    updates for infoTrack

--- InfoTrack.Seo.Web/InfoTrack.Seo.Web/App_Start/AuthConfig.cs
+++ InfoTrack.Seo.Web/InfoTrack.Seo.Web/App_Start/AuthConfig.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Web.WebPages.OAuth;
 using InfoTrack.Seo.Web.Models;
 
 namespace InfoTrack.Seo.Web
--- InfoTrack.Seo.Web/InfoTrack.Seo.Web/Controllers/HomeController.cs
+++ InfoTrack.Seo.Web/InfoTrack.Seo.Web/Controllers/HomeController.cs
@@ -9,15 +9,19 @@ using System.Text.RegularExpressions;
 using System.IO;
 using System.Text;
 using HtmlAgilityPack;
+using log4net;
 
 
 namespace InfoTrack.Seo.Web.Controllers
 {
     public class HomeController : Controller
     {
+
+        public static log4net.ILog _logger = log4net.LogManager.GetLogger(typeof(HomeController));
+
         public ActionResult Index()
         {
-
+            
             SearchModel model = new SearchModel();
             return View(model);
         }
@@ -41,54 +45,65 @@ namespace InfoTrack.Seo.Web.Controllers
 
         public static List<int> GetPosition(string url, string searchTerm)
         {
-            string raw = "http://www.google.com/search?q={0}";
-            string search = string.Format(raw, HttpUtility.UrlEncode(searchTerm));
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(search);
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            try
             {
-                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII))
+                string raw = "http://www.google.com/search?num=100&q={0}&btnG=Search";
+                string search = string.Format(raw, HttpUtility.UrlEncode(searchTerm));
+              
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(search);
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
                 {
-                    string html = reader.ReadToEnd();
-                    return FindPosition(html, url);
-                }
-            }
-        }
-
-
-        private static List<int> FindPosition(string html, string url)
-        {
-
-            var doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(html);
-
-            var nodes = doc.DocumentNode.SelectNodes("//h3/a[@href]");
-            int counter = -1;
-            List<int> positions = new List<int>();
-
-            if (nodes != null)
-            {
-                foreach (var node in nodes)
-                {
-                    counter++;
-
-                    foreach (var attribute in node.Attributes)
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.ASCII))
                     {
-                        if (attribute.Name == "href" && attribute.Value.Contains(url))
-                        {
-                            positions.Add(counter);
-
-                            return positions;
-                            
-                        }      
+                        string html = reader.ReadToEnd();
+                        return FindPosition(html, url);
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.ToString());  
             }
 
             return null;
         }
 
 
+        private static List<int> FindPosition(string html, string url)
+        {
+            try
+            {
+                var doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
 
+                var nodes = doc.DocumentNode.SelectNodes("//h3/a[@href]");
+                int counter = -1;
+                List<int> positions = new List<int>();
+
+                if (nodes != null)
+                {
+                    foreach (var node in nodes)
+                    {
+                        counter++;
+
+                        foreach (var attribute in node.Attributes)
+                        {
+                            if (attribute.Value.Contains(url) && counter < 100)
+                            {
+                                positions.Add(counter);
+                            }
+                        }
+                    }
+
+                    return positions;
+                }
+            }
+            catch (Exception ex)
+            {
+                //
+            }
+
+            return null;
+        }
     }
 }
